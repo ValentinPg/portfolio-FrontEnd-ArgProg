c2804e89a9bbe355fe40a86d7f61ec818127de83
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2022-12-03 12:39:44 -0300

    creacion de componentes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BiografiaComponent } from './componentes/biografia/biografia.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { IdiomasComponent } from './componentes/idiomas/idiomas.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BiografiaComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    SkillsComponent,
+    IdiomasComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
